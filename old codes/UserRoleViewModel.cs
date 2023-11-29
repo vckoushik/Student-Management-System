248d2648d0adf8744964102602cf8f2db2ec3bf6
Koushik Siva <koushiksiva9@gmail.com>
2023-11-28 19:07:15 -0600

    Removed Useless files

--- old codes/UserRoleViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Student_Management_System.Models
-{
-    public class UserRoleViewModel
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public bool IsSelected { get; set; }
-    }
-}
